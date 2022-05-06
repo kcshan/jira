213a56d07c26f789a5c457d4d2a520f0fcb2a2dd
kongcs <kongcs@imotorlinx.com>
2022-05-06 17:25:55 +0800

    feat: search-panel

---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export const SearchPanel = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: ''
+  });
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch('').then(async response => {
+      if (resonse.ok) {
+        setList(await resonse.json());
+      }
+    })
+  }, [param])
+
+  return <form>
+    <div>
+      <input type="text" value={param.name} onChange={evt => setParam({
+        ...param,
+        name: evt.target.value
+      })}/>
+      <select value={param.personId} onChange={evt => setParam({
+        ...param,
+        personId: evt.target.value
+      })}>
+        <option value={""}>负责人</option>
+        {
+          users.map((user) => {
+            return <option value={user.id}>user.name</option>
+          })
+        }
+      </select>
+    </div>
+  </form>
+};
