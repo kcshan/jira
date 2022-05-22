c65762daeea28451e149f87b4f3d2ddadca1b10d
kongcs <kongcs@imotorlinx.com>
2022-05-22 12:00:13 +0800

    fix: something

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import * as qs from "qs";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+
+// 使用js的同学，大部分错误都是在runtime（运行时）的时候发现的
+// 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 2000);
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
