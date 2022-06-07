5cfabdce7f12a61150e4b294f2649356dc27b371
kcshan <cshan.kong@gmail.com>
2022-06-07 10:55:51 +0800

    feat: middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { ProjectListScreen } from "./screens/project-list";
 import { TSReactTest } from "./try-use-array";
+import { LoginScreen } from "./screens/login";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
-      <TSReactTest />
+      <LoginScreen />
     </div>
   );
 }
