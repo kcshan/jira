d2827bd6af6d1e8a38a5854c9ed17d95baaaa9b6
kcshan <cshan.kong@gmail.com>
2022-06-11 15:46:48 +0800

    feat: 持久token

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -3,6 +3,7 @@ import * as qs from "qs";
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
 import { cleanObject, useDebounce, useMount } from "../../utils";
+import { useHttp } from "utils/http";
 
 // 使用js的同学，大部分错误都是在runtime（运行时）的时候发现的
 // 我们希望，在静态代码中，就能找到其中的一些错误 -> 强类型
@@ -16,23 +17,14 @@ export const ProjectListScreen = () => {
   });
   const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
+  const client = useHttp();
 
   useEffect(() => {
-    fetch(
-      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
-    ).then(async (response) => {
-      if (response.ok) {
-        setList(await response.json());
-      }
-    });
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiUrl}/users`).then(async (response) => {
-      if (response.ok) {
-        setUsers(await response.json());
-      }
-    });
+    client("users").then(setUsers);
   });
 
   return (
